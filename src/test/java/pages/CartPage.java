ff900116459af5407facb5207e1fa0c644e9a822
kalasnikovs.andrejs <a.kalasnikovs@reply.com>
2019-01-16 08:55:07 +0000

    All scenarios are passing. Component approach is used. Elements Not found during initialization are catched

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -5,6 +5,7 @@ import components.Link;
 import components.TextBox;
 import locators.CartLocators;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.pagefactory.AjaxElementLocatorFactory;
@@ -22,12 +23,26 @@ public class CartPage {
 
     public CartPage(BaseUtil base){
         //Initialize the page object
-        //PageFactory.initElements(new AjaxElementLocatorFactory(base.driver, 15), this);
+        PageFactory.initElements(new AjaxElementLocatorFactory(base.driver, 15), this);
         driver = base.driver;
 
         //Elements
-        dressInCart = new Label(base, driver.findElement(By.xpath(cartLocators.DRESS_IN_CART_XPATH)));
-        cartPageHeading = new Label(base, driver.findElement(By.xpath(cartLocators.CART_PAGE_HEADING_LABEL_XPATH)));
+        try {
+            dressInCart = new Label(base, driver.findElement(By.xpath(cartLocators.DRESS_IN_CART_XPATH)));
+        } catch (NoSuchElementException e) {
+            dressInCart=null;
+        } catch (Exception e) {
+            dressInCart=null;
+        }
+        //dressInCart = new Label(base, driver.findElement(By.xpath(cartLocators.DRESS_IN_CART_XPATH)));
+        try {
+            cartPageHeading = new Label(base, driver.findElement(By.xpath(cartLocators.CART_PAGE_HEADING_LABEL_XPATH)));
+        } catch (NoSuchElementException e) {
+            cartPageHeading=null;
+        } catch (Exception e) {
+            cartPageHeading=null;
+        }
+        //cartPageHeading = new Label(base, driver.findElement(By.xpath(cartLocators.CART_PAGE_HEADING_LABEL_XPATH)));
         logoutLink = new Link(base, driver.findElement(By.className(cartLocators.LOGOUT_LINK_CLASSNAME)));
     }
 
@@ -35,8 +50,12 @@ public class CartPage {
     public String cartContainsDressName(){
         try
         {
-            if(dressInCart.IsElementDisplayed())
-                return dressInCart.GetElementText();
+            if(dressInCart!=null) {
+                if (dressInCart.IsElementDisplayed())
+                    return dressInCart.GetElementText();
+                else
+                    return "Element not found";
+            }
             else
                 return "Element not found";
         }
@@ -48,7 +67,10 @@ public class CartPage {
 
     //Misc Business Logic
     public boolean isPageWithHeadingTextDisplayed(){
-        return cartPageHeading.IsElementDisplayed();
+        if(cartPageHeading!=null)
+            return cartPageHeading.IsElementDisplayed();
+        else
+            return false;
     }
 
     //Logout Business Logic
